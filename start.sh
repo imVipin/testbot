c267521e83b512ad18045978bd6fd56185f33e83
Esther-Lopez <66007532+Esther-Lopez@users.noreply.github.com>
2021-11-25 11:36:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Esther-Lopez/FindPDFbot.git /EvaMaria
+  git clone https://github.com/Esther-Lopez/FindPDFbot.git /FindPDFbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /FindPDFbot
 fi
-cd /EvaMaria
+cd /FindPDFbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
