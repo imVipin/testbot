bed74027fd227a142375dd9c080d536d775230f3
Esther-Lopez <66007532+Esther-Lopez@users.noreply.github.com>
2021-11-30 13:08:15 +0530

    Update extra_plugins.py

--- plugins/extra_plugins.py
+++ plugins/extra_plugins.py
@@ -12,18 +12,18 @@ UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "pdfmalayalam")
 @Client.on_message(filters.command(["telegraph"]))
 async def uploadphoto(client, message):
     if update.reply_to_message is not None:
-	 msg = await message.reply_text("`Tʀʏɪɴɢ Tᴏ Dᴏᴡɴʟᴏᴀᴅ`")
-         userid = str(message.chat.id)
-         img_path = (f"./DOWNLOADS/{userid}.jpg")
-         img_path = await client.download_media(message=message, file_name=img_path)
-         await msg.edit_text("`Tʀʏɪɴɢ Tᴏ Uᴘʟᴏᴀᴅ.....`")
-         try:
-           tlink = upload_file(img_path)
-         except:
-           await msg.edit_text("`Something went wrong`") 
-         else:
-           await msg.edit_text(f"https://telegra.ph{tlink[0]}")     
-           os.remove(img_path) 
+      msg = await message.reply_text("`Tʀʏɪɴɢ Tᴏ Dᴏᴡɴʟᴏᴀᴅ`")
+      userid = str(message.chat.id)
+      img_path = (f"./DOWNLOADS/{userid}.jpg")
+      img_path = await client.download_media(message=message, file_name=img_path)
+      await msg.edit_text("`Tʀʏɪɴɢ Tᴏ Uᴘʟᴏᴀᴅ.....`")
+      try:
+        tlink = upload_file(img_path)
+      except:
+        await msg.edit_text("`Something went wrong`") 
+      else:
+        await msg.edit_text(f"https://telegra.ph{tlink[0]}")     
+        os.remove(img_path) 
 	
     else:
 	await bot.send_message(text="reply  with photo")
